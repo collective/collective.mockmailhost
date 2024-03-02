0134782d8d12d7e93a4187bdb1732d85ca6b673b
Mauro Amico <mauro.amico@gmail.com>
2024-03-02 23:29:24 +0100

    flake8

--- collective/MockMailHost/testing.py
+++ collective/MockMailHost/testing.py
@@ -10,25 +10,24 @@ class CollectiveMockMailHostLayer(PloneSandboxLayer):
 
     def setUpZope(self, app, configurationContext):
         import collective.MockMailHost
+
         self.loadZCML(package=collective.MockMailHost)
 
     def setUpPloneSite(self, portal):
-        applyProfile(portal, 'collective.MockMailHost:default')
+        applyProfile(portal, "collective.MockMailHost:default")
 
 
 COLLECTIVE_MOCKMAILHOST_FIXTURE = CollectiveMockMailHostLayer()
 
 COLLECTIVE_MOCKMAILHOST_INTEGRATION_TESTING = IntegrationTesting(
-    bases=(COLLECTIVE_MOCKMAILHOST_FIXTURE, ),
-    name='CollectiveMockMailHostLayer:Integration'
+    bases=(COLLECTIVE_MOCKMAILHOST_FIXTURE,),
+    name="CollectiveMockMailHostLayer:Integration",
 )
 COLLECTIVE_MOCKMAILHOST_FUNCTIONAL_TESTING = FunctionalTesting(
-    bases=(COLLECTIVE_MOCKMAILHOST_FIXTURE, ),
-    name='CollectiveMockMailHostLayer:Functional'
+    bases=(COLLECTIVE_MOCKMAILHOST_FIXTURE,),
+    name="CollectiveMockMailHostLayer:Functional",
 )
 
 optionflags = (
-    doctest.REPORT_ONLY_FIRST_FAILURE
-    | doctest.ELLIPSIS
-    | doctest.NORMALIZE_WHITESPACE
+    doctest.REPORT_ONLY_FIRST_FAILURE | doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE  # noqa E501
 )
